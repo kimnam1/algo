50728438ca35b2e3abb74df7ed288f2ca8139e90
KimNamil <55401148+kimnam1@users.noreply.github.com>
2023-12-04 18:14:59 +0900

    BJ 231204

---
+++ 2588.py
@@ -0,0 +1,8 @@
+import sys
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+bString = str(b)
+print(a * int(bString[2]))
+print(a * int(bString[1]))
+print(a * int(bString[0]))
+print(a*b)
